3a9878813c63ea7a9f48cd5d6c0686716940fbaf
twozeronine <amdusias209@gmail.com>
2021-05-17 18:07:54 +0900

    Player.cs / 총알 발사 기능 리팩토링

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
   public float speed;
-  public float power;
+  public int power;
   public float maxShotDelay;
   public float curShotDelay;
 
-
   public GameObject bulletObjA;
   public GameObject bulletObjB;
+
+
   Animator anim;
 
   void Awake()
@@ -35,33 +36,22 @@ public class Player : MonoBehaviour
       return;
 
 
-
+    Func<string, Vector3, GameObject> GetBullet = (BulletName, offset) => Instantiate(Resources.Load<GameObject>("Prefabs/" + BulletName), transform.position + offset, transform.rotation);
+    Action<GameObject> BulletFire = (gameobject) => gameobject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
     switch (power)
     {
       case 1:
         //Power One
-        GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
-        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
-        rigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        BulletFire(GetBullet("Player_Bullet_A", Vector3.zero));
         break;
       case 2:
-        GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
-        GameObject bulletL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.1f, transform.rotation);
-        Rigidbody2D rigidbodyR = bulletR.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidbodyL = bulletL.GetComponent<Rigidbody2D>();
-        rigidbodyR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-        rigidbodyL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        BulletFire(GetBullet("Player_Bullet_A", Vector3.right * 0.1f));
+        BulletFire(GetBullet("Player_Bullet_A", Vector3.left * 0.1f));
         break;
       case 3:
-        GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.35f, transform.rotation);
-        GameObject bulletCC = Instantiate(bulletObjB, transform.position, transform.rotation);
-        GameObject bulletLL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.35f, transform.rotation);
-        Rigidbody2D rigidbodyRR = bulletRR.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidbodyCC = bulletCC.GetComponent<Rigidbody2D>();
-        Rigidbody2D rigidbodyLL = bulletLL.GetComponent<Rigidbody2D>();
-        rigidbodyRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-        rigidbodyCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-        rigidbodyLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        BulletFire(GetBullet("Player_Bullet_A", Vector3.right * 0.35f));
+        BulletFire(GetBullet("Player_Bullet_B", Vector3.zero));
+        BulletFire(GetBullet("Player_Bullet_A", Vector3.left * 0.35f));
         break;
     }
 
@@ -84,4 +74,5 @@ public class Player : MonoBehaviour
   {
     curShotDelay = Mathf.Clamp(curShotDelay + Time.deltaTime, 0, 0.3f);
   }
+
 }
