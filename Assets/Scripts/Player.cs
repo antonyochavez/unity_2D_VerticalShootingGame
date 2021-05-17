33e1b1a7c01eb13882518ff520ff06a63a399b82
twozeronine <amdusias209@gmail.com>
2021-05-17 14:39:29 +0900

    Player.cs / 플레이어 이동 모션

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
   public float speed;
+  Animator anim;
+
+  void Awake()
+  {
+    anim = GetComponent<Animator>();
+  }
   void Update()
   {
     float h = Input.GetAxisRaw("Horizontal");
     float v = Input.GetAxisRaw("Vertical");
     Vector3 nextPos = transform.position + new Vector3(h, v, 0).normalized * speed * Time.deltaTime;
     transform.position = new Vector3(Mathf.Clamp(nextPos.x, -2.2f, 2.2f), Mathf.Clamp(nextPos.y, -4.5f, 4.5f), nextPos.z);
+
+    if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+    {
+      anim.SetInteger("Input", (int)h);
+    }
+
   }
 }
