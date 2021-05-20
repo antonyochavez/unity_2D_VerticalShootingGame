4d9e503b5a441af88ff2be67688e4d6d7172a7ac
twozeronine <amdusias209@gmail.com>
2021-05-20 10:37:10 +0900

    Player.cs / 모바일 UI 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,10 @@ public class Player : MonoBehaviour
   public bool isHit;
   public bool isBoomTime;
 
+  public bool[] joyControl;
+  public bool isControl;
+  public bool isButtonA;
+  public bool isButtonB;
 
   public GameObject[] followers;
 
@@ -40,9 +44,22 @@ public class Player : MonoBehaviour
     Reload();
   }
 
+  public void JoyPanel(int type)
+  {
+    for (int i = 0; i < 9; i++)
+    {
+      joyControl[i] = i == type;
+    }
+  }
+
+  public void JoyDown() => isControl = true;
+  public void JoyUp() => isControl = false;
+
   void Fire()
   {
-    if (!Input.GetButton("Fire1"))
+    // if (!Input.GetButton("Fire1"))
+    //   return;
+    if (!isButtonA)
       return;
 
     if (curShotDelay < maxShotDelay)
@@ -86,8 +103,26 @@ public class Player : MonoBehaviour
   }
   void Move()
   {
+
+
+    //#.Keyboard Control Value
     float h = Input.GetAxisRaw("Horizontal");
     float v = Input.GetAxisRaw("Vertical");
+
+    //#.Joy Control Value
+    if (joyControl[0]) { h = -1; v = 1; }
+    if (joyControl[1]) { h = 0; v = 1; }
+    if (joyControl[2]) { h = 1; v = 1; }
+    if (joyControl[3]) { h = -1; v = 0; }
+    if (joyControl[4]) { h = 0; v = 0; }
+    if (joyControl[5]) { h = 1; v = 0; }
+    if (joyControl[6]) { h = -1; v = -1; }
+    if (joyControl[7]) { h = 0; v = -1; }
+    if (joyControl[8]) { h = 1; v = -1; }
+
+    if (!isControl)
+      return;
+
     Vector3 nextPos = transform.position + new Vector3(h, v, 0).normalized * speed * Time.deltaTime;
     transform.position = new Vector3(Mathf.Clamp(nextPos.x, -2.2f, 2.2f), Mathf.Clamp(nextPos.y, -4.5f, 4.5f), nextPos.z);
 
@@ -97,9 +132,15 @@ public class Player : MonoBehaviour
     }
   }
 
+  public void ButtonADown() => isButtonA = true;
+  public void ButtonAUp() => isButtonA = false;
+  public void ButtonBDown() => isButtonB = true;
+
   void Boom()
   {
-    if (!Input.GetButton("Fire2"))
+    // if (!Input.GetButton("Fire2"))
+    //   return;
+    if (!isButtonB)
       return;
     if (isBoomTime)
       return;
@@ -120,7 +161,7 @@ public class Player : MonoBehaviour
     GameObject[] enemiesL = objectManager.GetPool("EnemyL");
     GameObject[] enemiesM = objectManager.GetPool("EnemyM");
     GameObject[] enemiesS = objectManager.GetPool("EnemyS");
-    GameObject[] enemiesBoss = objectManager.GetPool("Enemy_B");
+    GameObject[] enemiesBoss = objectManager.GetPool("EnemyB");
 
     foreach (GameObject enemy in enemiesL)
     {
